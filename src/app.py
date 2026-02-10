70e1796a33ffb1623e62e96ce1f66d518a55d46e
PzD3v <ppesantez97@gmail.com>
2026-02-10 16:40:12 -0500

    Datastructures init

--- src/app.py
+++ src/app.py
@@ -5,16 +5,15 @@ import os
 from flask import Flask, request, jsonify, url_for
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
-from datastructures import FamilyStructure
+from datastructures import BankAccount
 # from models import Person
 
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
-CORS(app)
+CORS(app) # Con el Cors se puede acceder a la API desde otros dominios sin ello NO
 
-# Create the jackson family object
-jackson_family = FamilyStructure("Jackson")
+# Create object
 
 
 # Handle/serialize errors like a JSON object
@@ -28,34 +27,6 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-
-@app.route('/members', methods=['GET'])
-def handle_hello():
-    # This is how you can use the Family datastructure by calling its methods
-    members = jackson_family.get_all_members()
-    response_body = {"family": members}
-    return jsonify(response_body), 200
-
-@app.route('/member', methods=['POST'])
-def add_member():
-    member=request.json #obtener el diccionario que el POST envía
-    jackson_family.add_member(member) #agregar el miembro
-    return jsonify([{"msg":"Miembro agregado"}]), 200 #retornar la respuesta si todo esta bien 
-
-@app.route('/member/<int:id>', methods=["GET"])
-def get_member(id):
-    member = jackson_family.get_member(id)
-    return jsonify(member), 200
-
-@app.route('/member/<int:id>', methods=['DELETE'])
-def delete_member(id):
-    
-    success = jackson_family.delete_member(id)
-    if success:
-        members = jackson_family.get_all_members()
-        return jsonify({"msg": f"El miembro con id {id} ha sido eliminado"},{"family":members}),200
-    else:
-        return jsonify({"msg":"No se encontro un miembro con ese ID "})
 
 # This only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/datastructures.py
+++ src/datastructures.py
@@ -5,66 +5,26 @@ Update this file to implement the following already declared methods:
 - get_member: Should return a member from the self._members list
 """
 
-class FamilyStructure:
-    
-    def __init__(self, last_name):
-        self.last_name = last_name
-        self._next_id = 1
-        self._members = [
-            {
-                "id": self._generate_id(),
-                "first_name": "John",
-                "last_name": last_name,
-                "age": 33,
-                "lucky_numbers": [7, 13, 22]
-            },
-            {
-                "id": self._generate_id(),
-                "first_name": "Jane",
-                "last_name": last_name,
-                "age": 35,
-                "lucky_numbers": [10, 14, 3]
-            },
-            {
-                "id": self._generate_id(),
-                "first_name": "Jimmy",
-                "last_name": last_name,
-                "age": 5,
-                "lucky_numbers": [1]
-            }
-        ]
-
-    # This method generates a unique incremental ID
-    def _generate_id(self):
-        generated_id = self._next_id
-        self._next_id += 1
-        return generated_id
-
-    def add_member(self, member):
-        ## You have to implement this method
-        ## Append the member to the list of _members
-        member["id"] = self._generate_id()
-        member["last_name"] = self.last_name
-        self._members.append(member)
+class BankAccount:
+    def __init__(self,owner,pin):
+        self.owner = owner
+        self.balance = 0
+        self.__pin = pin #En Python, usar doble guion bajo __ hace que el atributo sea privado, nadie puede verlo desde fuera fácilmente).
         
-    def get_member(self, id):
-        ## You have to implement this method
-        ## Loop all the members and return the one with the given id
-        for person in self._members:
-            if person["id"] == id:
-                return person
-        return None
-
-    def delete_member(self, id):
-        ## You have to implement this method
-        ## Loop the list and delete the member with the given id
-        for person in self._members:
-            if person["id"]==id:
-                self._members.remove(person)
-                return self._members
-        return None
-
-
-    # This method is done, it returns a list with all the family members
-    def get_all_members(self):
-        return self._members
+    def deposit(self,amount):
+        self.balance += amount
+        return self.balance
+    
+    def withdraw(self,amount,pin_input):
+        if pin_input != self.__pin:
+            return "PIN INCORRECTO"
+        if amount > self.balance:
+            return "FONDOS_INSUFICIENTES"
+        self.balance -= amount
+        return self.balance
+    
+    def get_balance(self,pin_input):
+        if pin_input == self.__pin:
+            return self.balance
+        else:
+            None
